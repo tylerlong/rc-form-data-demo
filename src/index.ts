841963bec44e3544a720a25c17b0e2183200f2d6
Tyler Long <tyler4long@gmail.com>
2023-01-06 11:42:40 -0800

    dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('hello world');
+import axios from 'axios';
+
+const server = process.env.RINGCENTRAL_SERVER_URL!;
+const clientId = process.env.RINGCENTRAL_CLIENT_ID!;
+const clientSecret = process.env.RINGCENTRAL_CLIENT_SECRET!;
+const jwtToken = process.env.RINGCENTRAL_JWT_TOKEN!;
+const phoneNumber = process.env.PHONE_NUMBER!;
+
+console.log(server, clientId, clientSecret, jwtToken, phoneNumber);
