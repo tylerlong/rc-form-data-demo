26bd151f8ed7a490f4da295075214b04574027e0
Tyler Long <tyler4long@gmail.com>
2023-01-06 11:49:46 -0800

    Access token

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import * as querystring from 'querystring';
 
 const server = process.env.RINGCENTRAL_SERVER_URL!;
 const clientId = process.env.RINGCENTRAL_CLIENT_ID!;
@@ -6,4 +7,20 @@ const clientSecret = process.env.RINGCENTRAL_CLIENT_SECRET!;
 const jwtToken = process.env.RINGCENTRAL_JWT_TOKEN!;
 const phoneNumber = process.env.PHONE_NUMBER!;
 
-console.log(server, clientId, clientSecret, jwtToken, phoneNumber);
+const main = async () => {
+  const r = await axios.post(
+    `${server}/restapi/oauth/token`,
+    querystring.stringify({
+      grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+      assertion: jwtToken,
+    }),
+    {
+      auth: {
+        username: clientId,
+        password: clientSecret,
+      },
+    }
+  );
+  console.log(r.data);
+};
+main();
