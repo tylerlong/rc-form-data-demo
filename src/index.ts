6ed0f11fbc927e2aaf80ad667d1ae50c671fd132
Tyler Long <tyler4long@gmail.com>
2023-01-07 20:40:13 -0800

    support ReadableStream

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import axios from 'axios';
 import * as querystring from 'querystring';
 import * as fs from 'fs';
+import {Stream} from 'stream';
 
 const server = process.env.RINGCENTRAL_SERVER_URL!;
 const clientId = process.env.RINGCENTRAL_CLIENT_ID!;
 const clientSecret = process.env.RINGCENTRAL_CLIENT_SECRET!;
 const jwtToken = process.env.RINGCENTRAL_JWT_TOKEN!;
 const phoneNumber = process.env.PHONE_NUMBER!;
+
+async function stream2buffer(stream: Stream): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    const _buf = Array<any>();
+    stream.on('data', chunk => _buf.push(chunk));
+    stream.on('end', () => resolve(Buffer.concat(_buf)));
+    stream.on('error', err => reject(`error converting stream - ${err}`));
+  });
+}
 
 const main = async () => {
   const r = await axios.post(
@@ -45,18 +55,18 @@ const main = async () => {
       fileBuffer = Buffer.from(await content.arrayBuffer());
     } else {
       // NodeJS.ReadableStream
-      fileBuffer = content.read() as Buffer;
+      fileBuffer = await stream2buffer(content);
     }
     formData = Buffer.concat([formData, fileBuffer]);
   };
 
-  appendFile(
+  await appendFile(
     'request.json',
     'application/json',
-    `{"to": [{"phoneNumber": "${phoneNumber}"}]}`
+    `{"from":{"phoneNumber":"${phoneNumber}"},"to":[{"phoneNumber":"${phoneNumber}"}]}`
   );
-  appendFile('test.txt', 'text/plain', 'Hello world!');
-  appendFile('test.png', 'image/png', fs.readFileSync('test.png'));
+  // await appendFile('test.txt', 'text/plain', 'Hello world!');
+  await appendFile('test.png', 'image/png', fs.createReadStream('test.png'));
 
   const payload = Buffer.concat([
     formData,
@@ -64,7 +74,7 @@ const main = async () => {
   ]);
 
   const r2 = await axios.post(
-    `${server}/restapi/v1.0/account/~/extension/~/fax`,
+    `${server}/restapi/v1.0/account/~/extension/~/mms`,
     payload,
     {
       headers: {
